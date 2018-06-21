e11ac0100f2810f473a22adcc6588fbc5a01ae96
Steven Lang <steven.lang.mz@gmail.com>
2018-06-22 10:09:44 +1200

    Update documentation

--- fix-docs.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-find ./build/ -type f -name index.md -exec sed -i 's/\.`operator fun/\.<br>`operator fun/g' {} \;
-find ./build/ -type f -name index.md -exec sed -i 's/\.`fun/\.<br>`fun/g' {} \;
-
-echo "Finished fixing dokka generated docs"
